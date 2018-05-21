8cf485216049cb0364db2a3204e58e1dd5de56c0
Daniel Tooke <daniel.tooke@gmail.com>
2018-05-21 13:01:21 +0100

    Added simplecov to Rspec and initializing accounts with a balance

--- lib/account.rb
+++ lib/account.rb
@@ -2,4 +2,8 @@ class Account
 
   attr_reader :balance
 
+  def initialize(balance = 0)
+    @balance = balance
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,8 +2,14 @@ require 'account'
 
 describe Account do
 
+  subject(:account) {Account.new(1000)}
+
   it 'should have an accessible instance variable for balance' do
     expect(subject).to respond_to (:balance)
+  end
+
+  it 'should be able to be initialized with a certain balance' do
+    expect(account.balance).to eq (1000)
   end
   
 end
